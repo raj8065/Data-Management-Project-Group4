a92fe40175b0e6ddc932b8849caa6a4561b5360b
ReeseAEJones <ReeseAEJones@Gmail.com>
2019-03-24 14:25:33 -0400

    started enum

--- src/main/java/Dealer_Management_Program/User_Interface.java
+++ src/main/java/Dealer_Management_Program/User_Interface.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class User_Interface {
 
+    Enum UserType =
+
     public static void main(String[] args) {
         displayStartupMessage();
         Scanner scanner = new Scanner(System.in);
