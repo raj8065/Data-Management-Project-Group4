c18cb808b2dd4878bf86a08f1cba78211cf4f45f
ldy8497@rit.edu <ldy8497@rit.edu>
2019-03-24 15:19:15 -0400

    constructor for CommandConstructor

---
+++ src/main/java/Dealer_Management_Program/CommandConstructor.java
@@ -0,0 +1,13 @@
+package Dealer_Management_Program;
+
+public class CommandConstructor {
+
+    private String userInput;
+
+    public CommandConstructor(String userInput){
+
+        this.userInput = userInput;
+
+    }
+
+}
