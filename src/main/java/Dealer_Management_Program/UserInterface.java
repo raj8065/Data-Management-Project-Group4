2458f139e3802e67532be4d7971a36f62cd9677b
ReeseAEJones <ReeseAEJones@Gmail.com>
2019-03-24 20:09:12 -0400

    Made Things look better

--- src/main/java/Dealer_Management_Program/UserInterface.java
+++ src/main/java/Dealer_Management_Program/UserInterface.java
@@ -100,22 +100,21 @@ public class UserInterface {
 
     private static void displayResult(ResultSet result) throws SQLException {
 
-        if(result) {
-            int amnt = result.getMetaData().getColumnCount();
 
+        int amnt = result.getMetaData().getColumnCount();
+
+        for (int i = 0; i < amnt; i++)
+            System.out.print(result.getMetaData().getColumnName(i + 1) + " ");
+        System.out.println();
+        System.out.println("-----------------------------------------------------");
+
+        while (result.next()) {
             for (int i = 0; i < amnt; i++)
-                System.out.print(result.getMetaData().getColumnName(i + 1) + " ");
+                System.out.print(result.getObject(i + 1).toString() + " ");
+
             System.out.println();
-            System.out.println("-----------------------------------------------------");
+        }
 
-            while (result.next()) {
-                for (int i = 0; i < amnt; i++)
-                    System.out.print(result.getObject(i + 1).toString() + " ");
-
-                System.out.println();
-            }
-        } else
-            System.out.println("Command executed successfully.");
 
     }
 
